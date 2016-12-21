093776416dbb7803cf2f67599da3d18ba39ed926
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-12-21 11:02:10 +0100

    Removed output of args

--- aurora-git-version-cli/src/main/java/ske/aurora/gitversion/Main.java
+++ aurora-git-version-cli/src/main/java/ske/aurora/gitversion/Main.java
@@ -2,7 +2,6 @@ package ske.aurora.gitversion;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public final class Main {
     private Main() {
@@ -10,7 +9,6 @@ public final class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Arrays.asList(args).forEach(System.out::println);
         String path = ".";
         if (args.length == 1) {
             path = args[0];
