07d9478f8b47fa98fba383731b94670aa4ed8260
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-12-22 11:18:52 +0100

    Refactorings

--- aurora-git-version/src/main/java/ske/aurora/gitversion/GitVersion.java
+++ aurora-git-version/src/main/java/ske/aurora/gitversion/GitVersion.java
@@ -5,11 +5,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class GitVersion {
@@ -59,9 +56,9 @@ public class GitVersion {
     }
 
     protected Version determineVersion() throws IOException {
-        ObjectId head = repository.resolve("HEAD");
-        Optional<String> currentBranchName = getBranchName(head);
+        Optional<String> currentBranchName = getCurrentBranchName();
 
+        ObjectId head = repository.resolve("HEAD");
         Optional<String> versionTagOnHead = getVersionTagOnCommit(head);
 
         return versionTagOnHead
@@ -93,79 +90,10 @@ public class GitVersion {
             .findFirst();
     }
 
-    protected Optional<String> getBranchName(ObjectId commitId) throws IOException {
+    protected Optional<String> getCurrentBranchName() throws IOException {
 
-        return getBranchName(this.repository, commitId, options.fallbackToBranchNameEnv,
-            options.fallbackBranchNameEnvName);
-    }
-
-    /**
-     * Determine the name of the current branch. The assumption is that head of the current branch is
-     * <code>commitId</code> and that commit will be used to determine if we are in detached head state. If we are
-     * in detached head state, we will fall back to the value of the environment variable
-     * <code>fallbackBranchNameEnvName</code> to use as branch name.
-     *
-     * @param repository
-     * @param commitId
-     * @param fallbackToBranchNameEnv
-     * @param fallbackBranchNameEnvName
-     * @return
-     * @throws IOException
-     */
-    public static Optional<String> getBranchName(Repository repository, ObjectId commitId,
-        boolean fallbackToBranchNameEnv, String fallbackBranchNameEnvName) throws IOException {
-
-        String currentBranchName = repository.getBranch();
-
-        boolean isDetachedHead = commitId.getName().equals(currentBranchName);
-        if (!isDetachedHead) {
-            return Optional.of(currentBranchName);
-        }
-
-        return getBranchNameFromDetachedHead(repository, commitId, fallbackToBranchNameEnv, fallbackBranchNameEnvName);
-    }
-
-    /**
-     * If we are trying to determine the branch name of the current commit when we are in detached head
-     * state, we need to resort to either hints or heuristics. This method will first check for the presence of
-     * an environment variable called <code>options.fallbackBranchNameEnvName</code> (default BRANCH_NAME). If it
-     * exists, its value will be used as branch name (Jenkins sets this environment variable before performing a
-     * build).
-     * <p>
-     * If the environment variable is not set we have to resort to a broad search for the commit. We pick the first
-     * branch we find the commit in.
-     *
-     * @param repository
-     * @param commitId
-     * @param fallbackToBranchNameEnv
-     * @param fallbackBranchNameEnvName
-     * @return
-     * @throws IOException
-     */
-    protected static Optional<String> getBranchNameFromDetachedHead(Repository repository, ObjectId commitId,
-        boolean fallbackToBranchNameEnv,
-        String fallbackBranchNameEnvName) throws IOException {
-
-        if (fallbackToBranchNameEnv) {
-            String branchNameFromEnv = System.getenv(fallbackBranchNameEnvName);
-            if (branchNameFromEnv != null) {
-                return Optional.of(branchNameFromEnv);
-            }
-        }
-
-        RevWalk walk = new RevWalk(repository);
-        RevCommit commit = walk.parseCommit(repository.resolve(commitId.getName() + "^0"));
-        return repository.getAllRefs().entrySet().stream()
-            .filter(e -> e.getKey().startsWith(Constants.R_HEADS))
-            .filter(e -> {
-                try {
-                    return walk.isMergedInto(commit, walk.parseCommit(e.getValue().getObjectId()));
-                } catch (IOException e1) {
-                    return false;
-                }
-            })
-            .map(e -> e.getValue().getName().replaceFirst("refs/heads/", ""))
-            .findFirst();
+        return GitTools
+            .getBranchName(this.repository, options.fallbackToBranchNameEnv, options.fallbackBranchNameEnvName);
     }
 
     public enum VersionSource {
