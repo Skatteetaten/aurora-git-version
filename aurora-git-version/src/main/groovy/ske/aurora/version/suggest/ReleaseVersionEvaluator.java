1dba527ce24139fe9fb7bfb8fe0f3f17bcdd4660
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2017-01-03 06:48:21 +0100

    Converted ReleaseVersionEvaluator to java

---
+++ aurora-git-version/src/main/groovy/ske/aurora/version/suggest/ReleaseVersionEvaluator.java
@@ -0,0 +1,27 @@
+package ske.aurora.version.suggest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class ReleaseVersionEvaluator {
+    VersionNumber currentVersion;
+
+    public ReleaseVersionEvaluator(String versionNumber) {
+        this.currentVersion = VersionNumber.parse(versionNumber);
+    }
+
+    VersionNumber suggestNextReleaseVersionFrom(List<String> listOfVersions) {
+        List<VersionNumber> orderedListOfEligibleVersions = listOfVersions.stream()
+            .filter(VersionNumber::isValid)
+            .map(VersionNumber::parse)
+            .sorted()
+            .filter(currentVersion::canBeUsedWhenDeterminingReleaseVersion)
+            .collect(Collectors.toList());
+        if (orderedListOfEligibleVersions.isEmpty()) {
+            return currentVersion.unlockVersion();
+        } else {
+            return orderedListOfEligibleVersions.get(orderedListOfEligibleVersions.size() - 1).adaptTo(currentVersion)
+                .incrementLastSegment();
+        }
+    }
+}
